be7c985bfb87b457369ba6151c3ec9e7163035e3
Juel B <juel.braganza97@gmail.com>
2021-04-07 01:50:22 +0100

    update script for new API

--- script.js
+++ script.js
@@ -1,59 +1,90 @@
 // Defining a baseURL and token to as part of the request URL
-
-const baseURL = 'https://api-ssl.bitly.com/v4/shorten';
-const token = 'dc95214bb1e708659bad95230db67b876c6df93d';
+const baseURL = 'https://api.shrtco.de/v2/shorten';
 let url;
 
-// Grab references to all the DOM elements you'll need to manipulate
-
-const longURL = document.querySelector('.search');
+// Select the DOM elements
+const longURL = document.querySelector('.long-url');
 const searchForm = document.querySelector('form');
+const waitMsg = document.querySelector('.message')
+const resultDiv = document.querySelector('.result');
+const resultSection = document.querySelector('section');
 
-const section = document.querySelector('section');
-
-// Event listeners to control the functionality
+// Submit form
 searchForm.addEventListener('submit', fetchResults);
 
-function fetchResults(e) {
-    // Use preventDefault() to stop the form submitting
-    e.preventDefault();
-    
-    var raw = "{\n    \"long_url\": \"<string>\",\n    \"group_guid\": \"<string>\",\n    \"domain\": \"bit.ly\"\n}";
+// create a 'p' element to display a wait message
+const para = document.createElement('p');
 
-    var requestOptions = {
-      method: 'POST',
-      body: raw,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      redirect: 'follow',
-      Authorization: 'dc95214bb1e708659bad95230db67b876c6df93d',
-    };
-    
-    fetch("https://api-ssl.bitly.com/v4/shorten", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
+
+function fetchResults(e) {
+
+  // Use preventDefault() to stop the form submitting
+  e.preventDefault();
+
+  // display a wait message
+  para.innerText = "Please wait a few moments to get your link..."
+  waitMsg.appendChild(para)
+
+  // assemble the request url
+  url = `${baseURL}?url=${longURL.value}`
+
+  // retrieve the JSON response
+  fetch(url)
+    .then(response => response.json())
+    .then(json => displayResults(json))
+    .catch(error => console.log('error', error));
 
 }
 
 function displayResults(json) {
 
-    const link = json.response.docs;
+  // remove wait message
+  para.style.display = 'none'
 
-    const article = document.createElement('article');
-    const heading = document.createElement('h2');
-    const para2 = document.createElement('p');
-    const clearfix = document.createElement('div');
+  // Retrieve the original & shortened link from the JSON response
+  const shortLink = json.result.full_short_link;
+  const originalLink = json.result.original_link;
 
-    article.href = link.long_url;
+  // ?
+  const clearfix = document.createElement('div');
+  clearfix.setAttribute('class', 'clearfix');
 
-    clearfix.setAttribute('class', 'clearfix');
+  // display the results to the user
+  resultSection.innerHTML = `
+  <div class="response">
+    <span class="long-link">${originalLink}</span>
+    <div class="row-right">
+      <p class="short-link">${shortLink}</p>
+      <button class="copy-btn" id="copy-btn" data-link="link-">Copy!</button>
+    <div>
+  </div>`;
 
-    article.appendChild(heading);
-    heading.appendChild(link);
-    article.appendChild(para2);
-    article.appendChild(clearfix);
-    section.appendChild(article);
+  // add copy to clipboard functionality to button
+  const copyBtn = document.querySelector('.copy-btn')
+  copyBtn.addEventListener('click', copyLink)
+
+  // clear the input field
+  longURL.value = ""
+
+  // clear?
+  article.appendChild(clearfix);
+
+}
+
+function copyLink() {
+
+  // change color when copied to clipboard -> indicates change to user
+  let copyBtnColor = this.style.backgroundColor;
+  this.style.backgroundColor = copyBtnColor ? '' : "#323232";
+  this.innerText = 'Copied!';
+
+  // As the link is in a 'p' tag, place its text in a textarea in order to copy to the clipboard
+  const shortLinkCopied = this.previousElementSibling.innerText;
+  var textArea = document.createElement('textarea');
+  textArea.value = shortLinkCopied;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  textArea.remove();
 
 }
